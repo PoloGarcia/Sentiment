04a251cb8a9fd087029e0491bd2f5cf911931a20
PoloGarcia <laga.1193@gmail.com>
2015-11-20 20:51:23 -0600

    Prevent request on testing phase

--- Sentiment.py
+++ Sentiment.py
@@ -76,5 +76,5 @@ def parseTweets(query,outputFile,parsedFile):
 	jsonFile.close()
 
 #Main()
-parseTweets('Bernie%20Sanders','bernard.txt','jsonBernie.data')
-parseTweets('Hillary%20Clinton','hillary.txt','jsonHillary.data')
+#parseTweets('Bernie%20Sanders','bernard.txt','jsonBernie.data')
+#parseTweets('Hillary%20Clinton','hillary.txt','jsonHillary.data')
